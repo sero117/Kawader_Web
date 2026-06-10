85907395e1f02efcda5411a4b7fbf6e72baf015a
Sarah <sero201910021@gmail.com>
2026-06-10 18:17:33 +0300

    Auto-generated commit

---
+++ chunk-6WEVVJF4.js
@@ -0,0 +1,1 @@
+var r=(e=>(e[e.Employee=0]="Employee",e[e.HumanResourceManager=1]="HumanResourceManager",e[e.DepartmentManager=2]="DepartmentManager",e[e.BranchManager=3]="BranchManager",e))(r||{});export{r as a};
