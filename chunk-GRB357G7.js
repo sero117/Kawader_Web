e66de7681fde5c0a54d29d6a4e52aff72c523b6c
sero117 <sero117@users.noreply.github.com>
2026-07-24 18:28:57 +0000

    deploy: 840bd0c433b46634f44e43d5884507436c42b7ac

--- chunk-GRB357G7.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-VKFKPTDW.js";import{Ma as i,R as t}from"./chunk-ZUV67EJU.js";var n=class r{lang=t(a);transform(e){return this.lang.t(e)}static \u0275fac=function(p){return new(p||r)};static \u0275pipe=i({name:"translate",type:r,pure:!1})};export{n as a};
