37042193739490f1ff7a51a329e42866c20f48f7
sero117 <sero117@users.noreply.github.com>
2026-07-31 11:04:07 +0000

    deploy: 3a6f0f822b9121a93e939757c60edbc2c971972b

--- chunk-DCDLX7NW.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-MOSES4XU.js";import{Na as i,S as t}from"./chunk-QMBQL72V.js";var n=class r{lang=t(a);transform(e){return this.lang.t(e)}static \u0275fac=function(p){return new(p||r)};static \u0275pipe=i({name:"translate",type:r,pure:!1})};export{n as a};
