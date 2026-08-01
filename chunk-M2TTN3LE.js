29c804cbb3b34b9ab7352cd9be94cfd3212fbc14
sero117 <sero117@users.noreply.github.com>
2026-08-01 08:37:16 +0000

    deploy: 02221eb2c56ec90084be30ba55de4550c7690925

---
+++ chunk-M2TTN3LE.js
@@ -0,0 +1,1 @@
+import{a as n}from"./chunk-IASD24SJ.js";import{a as t}from"./chunk-X5OLIND3.js";import{N as r,S as i}from"./chunk-HYSNRBO7.js";var a=class e{api=i(n);baseUrl=`${t.apiUrl}/Adms`;getLogs(){return this.api.get(`${this.baseUrl}/logs`)}static \u0275fac=function(o){return new(o||e)};static \u0275prov=r({token:e,factory:e.\u0275fac,providedIn:"root"})};export{a};
